b12b61fc06ce3515d0285fc487b19f57087cef23
Jacob Moss <jacobmoss09@gmail.com>
2019-08-17 09:47:47 -0500

    switch case structure built out for manager file

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,4 +1,4 @@
-var mysql      = require('mysql');
+var mysql = require('mysql');
 var inquirer = require('inquirer');
 
 
@@ -11,3 +11,46 @@ var connection = mysql.createConnection({
 });
  
 connection.connect();
+
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    {
+        "type": "list",
+        "choices": ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"],
+        "name": "availableOptions",
+        "message": "Welcome manager. What would you like to do?"
+    }
+  ])
+  .then(answers => {
+    // Use user feedback for... whatever!!
+    //console.log(answers.availableOptions);
+    switch(answers.availableOptions) {
+        case "View Products for Sale":
+          // code block
+          console.log("you choose view products for sale");
+          break;
+        case "View Low Inventory":
+          // code block
+          console.log("you chose view low inventory")
+          break;
+        case "Add to Inventory":
+            // code block
+            console.log("you chose Add to Inventory")
+            break;
+        case "Add New Product":
+            // code block
+            console.log("you chose Add New Product")
+            break;
+        //default:
+          // code block
+
+      }
+
+
+
+
+
+
+
+  });
