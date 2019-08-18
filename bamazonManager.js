21cbb8ae38fc832925e56d641a16164ec0548e84
Jacob Moss <jacobmoss09@gmail.com>
2019-08-17 21:33:29 -0500

    submitting all current work

--- bamazonManager.js
+++ bamazonManager.js
@@ -13,50 +13,47 @@ var connection = mysql.createConnection({
 connection.connect();
 
 var timesRun = 0;
+if (timesRun===0) {
+  console.log("Welcome Managers!\n\n\n");
+};
 
 function managementOptions() {
-  if (timesRun===0) {
-    console.log("Welcome Managers!\n\n\n");
-  }
   timesRun++;
-
-
-inquirer
-  .prompt([
-    /* Pass your questions in here */
-    {
+  inquirer
+    .prompt([
+      /* Pass your questions in here */
+      {
         "type": "list",
         "choices": ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"],
         "name": "availableOptions",
         "message": "What would you like to do?"
-    }
-  ])
-  .then(answers => {
-    // Use user feedback for... whatever!!
-    //console.log(answers.availableOptions);
-    switch(answers.availableOptions) {
+      }
+    ])
+    .then(answers => {
+      // Use user feedback for... whatever!!
+      //console.log(answers.availableOptions);
+      switch(answers.availableOptions) {
         case "View Products for Sale":
           // code block
-          console.log("you choose view products for sale");
           viewProducts();
           break;
         case "View Low Inventory":
           // code block
-          console.log("you chose view low inventory")
+          viewLowInventory();
           break;
         case "Add to Inventory":
             // code block
-            console.log("you chose Add to Inventory")
+            addInventory();
             break;
         case "Add New Product":
             // code block
-            console.log("you chose Add New Product")
+            addNewItem();
             break;
         //default:
           // code block
 
-    }
-});
+      }
+  });
 };
 managementOptions();
 
@@ -64,10 +61,85 @@ managementOptions();
 function viewProducts(){
   connection.query('SELECT * FROM products', function (error, results, fields) {
     if (error) throw error;
-    //console.log(results);
+    console.log("\nHere are your items for sale:\n\n");
     results.forEach(element => {
-      console.log("Here are your items: \n\nID: " + element.item_id+ "\nname: " +element.product_name +  "\nprice: " + element.price + "\nquantity: " + element.stock_quantity +"\n--------------------\n");
+      console.log("ID: " + element.item_id+ "\nname: " +element.product_name +  "\nprice: " + element.price + "\nquantity: " + element.stock_quantity +"\n--------------------\n");
     });
   });
+  // connection.end();
   // managementOptions();
+};
+
+function viewLowInventory() {
+  connection.query('SELECT * FROM products', function (error, results, fields) {
+    if (error) throw error;
+    console.log("\nHere are your low inventory items:\n\n");
+    results.forEach(element => {
+      if (element.stock_quantity < 5 ) {
+        console.log("ID: " + element.item_id+ "\nname: " +element.product_name +  "\nprice: " + element.price + "\nquantity: " + element.stock_quantity +"\n--------------------\n");
+      } 
+    });
+  });
+  // connection.end();
+  // managementOptions();
+};
+
+
+function addInventory(){
+  connection.query('SELECT * FROM products', function (error, results, fields) {
+    if (error) throw error;
+    //PROMPT MANAGER TO SEE WHICH ITEM THEY'D LIKE TO ADD INVENTORY TO
+    inquirer
+    .prompt([
+      /* Pass your questions in here */
+      {
+        "name": "restockList",
+        "type": "rawlist",
+        "message": "Which item would you like to restock?",
+        "choices": function() {
+            var productNames=[];
+            results.forEach(function(e){
+              productNames.push(e.product_name);
+            });
+            return productNames;
+        }
+      },
+      {
+        "name": "restockQuantity",
+        "type": "number",
+        "message": "How many units would you like to add?"
+      }
+    ])
+    .then(answers => {
+      // Use user feedback for... whatever!!
+      //UPDATE DATABASE RECORD ACCORDING TO THE ITEM CHOSEN AND THE QUANTITY ADDED
+      var currentQuantity = "";
+      results.forEach(function(e){
+        if (e.product_name === answers.restockList) {
+          currentQuantity = e.stock_quantity;
+        }
+      })
+
+      connection.query('UPDATE products SET ? WHERE ?',
+      [
+        {
+          //NEED TO MAKE THIS QUERY FOR THIS SPECIFIC ITEM'S QUANT. THEN ADD USER INPUT.
+          "stock_quantity": currentQuantity + answers.restockQuantity
+        },
+        {
+          "product_name": answers.restockList
+        }
+      ], 
+      function (error, results, fields) {
+        if (error) throw error;
+        console.log('Adding ' + answers.restockQuantity  + " " +  answers.restockList + " units to your inventory");
+      });
+
+    });
+
+  });
+};
+
+function addNewItem() {
+  connection.query("INSERT INTO products VALUES")
 }
